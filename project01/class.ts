f038ea8096421351a84f5f34d5b493cdcbc64bcb
yinchen <yinchen@zhongan.com>
2022-05-06 10:09:35 +0800

    feat:typescript学习完成

--- project01/class.ts
+++ project01/class.ts
@@ -1,6 +1,6 @@
 class Animal {
-    name: string;
-
+    readonly name: string;
+    static categoies: string[] = ['mammal', 'bird'];
     constructor(name: string) {
         this.name = name
     }
@@ -9,5 +9,58 @@ class Animal {
         return `${this.name} is running`
     }
 }
+
+console.log(Animal.categoies);
 const snake = new Animal('lily')
 console.log(snake.run())
+// snake.name = 'lucy'
+console.log(snake.name)
+
+class Dog extends Animal {
+    bark() {
+        return `${this.name} is barking`
+    }
+}
+const xiaobao = new Dog('xx')
+console.log(xiaobao.bark())
+
+
+class Cat extends Animal {
+    constructor(name: string) {
+        super(name)
+        console.log(this.name)
+    }
+    run() {
+        return `Meow, ` + super.run()
+    }
+}
+
+const maomao = new Cat('MAOMAO');
+console.log(maomao.run())
+
+
+interface Radio {
+    switchRadio(tiggerL: boolean): void; 
+}
+
+interface Battery {
+    checkBatteryStatus();
+}
+
+interface RadioWithBattery extends Radio {
+    checkBatteryStatus();
+}
+class Car implements Radio {
+    switchRadio() {
+
+    }
+}
+
+class CellPhone implements RadioWithBattery {
+    switchRadio() {
+
+    }
+    checkBatteryStatus() {
+
+    }
+}
---
+++ project01/enums.ts
@@ -0,0 +1,11 @@
+enum Direction {
+    Up = 'UP',
+    Down = 'DOWM',
+    Left = 'LEFT',
+    Right = 'RIGHT',
+}
+const value = 'UP';
+if (value === Direction.Up) {
+    console.log('go up')
+}
+console.log(Direction.Up)
